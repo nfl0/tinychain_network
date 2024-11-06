ca91436548de51264f489cdfea5c814c3e1bf2d0
nfl0 <nawfalona@gmail.com>
2024-11-06 06:06:24 +0100

    improve del_dbs.sh

--- src/del_dbs.sh
+++ src/del_dbs.sh
@@ -3,12 +3,16 @@
 # List of directories to be removed
 directories=("transactions.db" "headers.db" "blocks.db" "states.db")
 
-# Iterate through the list and remove each directory and its contents
-for dir in "${directories[@]}"; do
-    if [ -d "$dir" ]; then
-        rm -r "$dir"
-        echo "Removed $dir"
-    else
-        echo "$dir does not exist"
-    fi
+# Iterate through node* directories
+for node_dir in node*/; do
+    echo "Processing $node_dir"
+    # Iterate through the list and remove each directory and its contents
+    for dir in "${directories[@]}"; do
+        if [ -d "$node_dir$dir" ]; then
+            rm -r "$node_dir$dir"
+            echo "Removed $dir in $node_dir"
+        else
+            echo "$dir does not exist in $node_dir"
+        fi
+    done
 done
