1968502d48902ad2f14c2d94bb2c571900a7b468
nfl0 <nawfalona@gmail.com>
2024-11-11 03:47:06 +0100

    added transaction_pool.py

--- src/tinychain/tinychain.py
+++ src/tinychain/tinychain.py
@@ -14,30 +14,12 @@ from vm import TinyVMEngine
 from wallet import Wallet
 from parameters import HTTP_PORT, MAX_TX_POOL, ROUND_TIMEOUT
 from peer_communication import broadcast_block_header, broadcast_transaction
+from transaction_pool import TransactionPool
 
 TINYCOIN = 1000000000000000000
 TINYCHAIN_UNIT = 'tatoshi'
 
 app = web.Application()
-    
-class TransactionPool:
-    def __init__(self, max_size):
-        self.transactions = {}
-        self.max_size = max_size
-
-    def add_transaction(self, transaction):
-        if len(self.transactions) >= self.max_size:
-            raise ValueError("Transaction pool is full")
-        if len(self.transactions) < self.max_size:
-            self.transactions[transaction.transaction_hash] = transaction
-    def remove_transaction(self, transaction):
-        self.transactions.pop(transaction.transaction_hash, None)
-    def get_transactions(self):
-        return sorted(self.transactions.values(), key=lambda tx: tx.fee, reverse=True)
-    def get_transaction_by_hash(self, hash):
-        return self.transactions[hash]
-    def is_empty(self):
-        return not self.transactions
 
 
 class Forger:
---
+++ src/tinychain/transaction_pool.py
@@ -0,0 +1,18 @@
+class TransactionPool:
+    def __init__(self, max_size):
+        self.transactions = {}
+        self.max_size = max_size
+
+    def add_transaction(self, transaction):
+        if len(self.transactions) >= self.max_size:
+            raise ValueError("Transaction pool is full")
+        if len(self.transactions) < self.max_size:
+            self.transactions[transaction.transaction_hash] = transaction
+    def remove_transaction(self, transaction):
+        self.transactions.pop(transaction.transaction_hash, None)
+    def get_transactions(self):
+        return sorted(self.transactions.values(), key=lambda tx: tx.fee, reverse=True)
+    def get_transaction_by_hash(self, hash):
+        return self.transactions[hash]
+    def is_empty(self):
+        return not self.transactions
